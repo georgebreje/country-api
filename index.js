f53e1194ae9de80770bab2d19ff71ef58ed2b922
George Breje <george.breje2@gmail.com>
2025-05-26 12:50:51 +0300

    reconfigure port

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const API_KEY = process.env.API_KEY;
-const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,6 +37,7 @@ app.get('/country/:countryCode', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000; // Use the port assigned by Render
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
